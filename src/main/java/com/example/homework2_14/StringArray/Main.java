e13aa211af1788fd959c4c88b1e1594861797121
ruslanzverkov <createmed@mail.ru>
2022-11-12 23:53:54 +0300

    1

---
+++ src/main/java/com/example/homework2_14/StringArray/Main.java
@@ -0,0 +1,12 @@
+package com.example.homework2_14.StringArray;
+
+import com.example.homework2_14.Application;
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+
--- src/main/java/com/example/homework2_14/StringArray/StringListImpl.java
+++ src/main/java/com/example/homework2_14/StringArray/StringListImpl.java
@@ -1,16 +1,38 @@
 package com.example.homework2_14.StringArray;
 
+import java.util.Objects;
+
 public class StringListImpl implements StringList {
+    private static final int INITIAL_SIZE=15;
+    private final String[] stringArray;
+    private int capacity;
+
+    public StringListImpl() {
+        stringArray = new String[INITIAL_SIZE];
+        capacity=0;
+    }
 
 
     @Override
     public String add(String item) {
+
+
         return null;
     }
 
     @Override
     public String add(int index, String item) {
-        return null;
+
+        if (capacity >= stringArray.length) {
+            throw new IllegalArgumentException("Список полон");
+        }
+        checkNotNull(item);
+        checkNonNegativeIndex(index);
+        checkIndex(index, false);
+        System.arraycopy(stringArray, index, stringArray, index + 1, capacity - index);
+        capacity++;
+        return stringArray[index] = item;
+
     }
 
     @Override
@@ -72,4 +94,26 @@ public class StringListImpl implements StringList {
     public String[] toArray() {
         return new String[0];
     }
+
+    private void checkNotNull(String string) {
+        if (Objects.isNull(string)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkNonNegativeIndex(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkIndex(int index, boolean includeEquality) {
+        boolean expression=includeEquality?index>=capacity:index>capacity;
+        if (expression) {
+            throw new IllegalArgumentException("индекс: "+index+"размер: "+capacity);
+        }
+
+    }
+
+
 }
