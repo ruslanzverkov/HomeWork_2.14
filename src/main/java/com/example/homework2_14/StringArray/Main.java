39f047f252ac99bc84a7bc5beab813e26046038b
ruslanzverkov <createmed@mail.ru>
2022-11-19 23:00:27 +0300

    4

--- src/main/java/com/example/homework2_14/StringArray/Main.java
+++ src/main/java/com/example/homework2_14/StringArray/Main.java
@@ -5,12 +5,7 @@ import org.springframework.boot.SpringApplication;
 
 public class Main {
     public static void main(String[] args) {
-        StringList stringList = new StringListImpl();
-        stringList.add("t1");
-        stringList.add("t2");
-        stringList.add("t3");
-        stringList.add("t4");
-        System.out.println(stringList);
+
     }
 
 }
--- src/test/java/com/example/homework2_14/StringListImplTest.java
+++ src/test/java/com/example/homework2_14/StringListImplTest.java
@@ -7,6 +7,7 @@ import com.example.homework2_14.StringArray.StringListImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -19,14 +20,19 @@ public class StringListImplTest {
         stringList.clear();
     }
 
-    @Test
-    public void addTest() {
-        String[] elements = new String[]{"t1", "t2", "t3", "t4"};
-        stringList.add("t1");
-        stringList.add("t2");
-        stringList.add("t3");
-        stringList.add("t4");
+    @BeforeEach
+    public void beforeEach() {
 
+        stringList.add("t1");// i-0
+        stringList.add("t2");// i-1
+        stringList.add("t3");// i-2
+        stringList.add("t4");// i-3
+    }
+
+    @Test
+    void addTest() {
+        String[] elements = new String[]{"t1", "t2", "t3", "t4"};
+        assertThat(stringList.toArray()).hasSize(elements.length);
         for (int i = 0; i < elements.length; i++) {
             assertThat(stringList.get(i)).isEqualTo(elements[i]);
             assertThat(stringList.contains(elements[i])).isTrue();
@@ -35,8 +41,48 @@ public class StringListImplTest {
 
         }
 
+    }
+
+    @Test
+    void removeTest() {
+        String[] elements = new String[]{"t1", "t2", "t3"};
+        stringList.remove("t4");
         assertThat(stringList.toArray()).hasSize(elements.length);
-        assertThat(stringList.toArray()).containsExactly(elements);
+
+        for (int i = 0; i < elements.length; i++) {
+            assertThat(stringList.get(i)).isEqualTo(elements[i]);
+            assertThat(stringList.indexOf(elements[i])).isEqualTo(i);
+            assertThat(stringList.contains(elements[i])).isTrue();
+            assertThat(stringList.lastIndexOf(elements[i])).isEqualTo(i);
+        }
+    }
+
+    @Test
+    void setTest() {
+        String[] elements = new String[]{"t1", "t5", "t3", "t4"};
+        stringList.set(1, "t5");
+        assertThat(stringList.toArray()).hasSize(elements.length);
+
+        for (int i = 0; i < elements.length; i++) {
+            assertThat(stringList.get(i)).isEqualTo(elements[i]);
+            assertThat(stringList.indexOf(elements[i])).isEqualTo(i);
+            assertThat(stringList.contains(elements[i])).isTrue();
+            assertThat(stringList.lastIndexOf(elements[i])).isEqualTo(i);
+        }
+    }
+
+    @Test
+    void equalsTest() {
+        String[] elements = new String[]{"t1", "t2", "t3", "t4"};
+        StringList stringList2=new StringListImpl();
+
+        for (String element : elements) {
+            stringList2.add(element);
+        }
+        assertThat(stringList.equals(stringList2)).isTrue();
+        stringList2.clear();
+        assertThat(stringList.equals(stringList2)).isFalse();
+
     }
 
 
